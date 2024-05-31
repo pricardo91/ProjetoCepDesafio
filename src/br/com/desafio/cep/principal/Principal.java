00bc8dd808e5f97419a9d3838f117f5adc749499
Ricardo Silva <pricardo.sribeiro@gmail.com>
2024-05-30 22:28:01 -0300

    [FEAT] Adding exit to end the program!

--- src/br/com/desafio/cep/principal/Principal.java
+++ src/br/com/desafio/cep/principal/Principal.java
@@ -11,27 +11,41 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Scanner leitura = new Scanner(System.in);
-		
+
 		ConsultaCep consultaCep = new ConsultaCep();
-		
-		System.out.println("Digite o CEP para pesquisar");
-		var cep = leitura.nextLine();
-		
-		try {
-		
-        Endereco novoEndereco = consultaCep.buscaEndereco(cep);
-        System.out.println(novoEndereco);
-        GeradorDeArquivo gerador = new GeradorDeArquivo();
-        gerador.salvaJson(novoEndereco);
-        
-		}catch (RuntimeException | IOException e) {
-			System.out.println("ERRO: " + e.getMessage());
-			System.out.println("Programa finalizado no catch da Principal!");
+
+		String cep = "";
+
+		while (!cep.equalsIgnoreCase("Sair")) {
+			
+			System.out.println("Digite o CEP para pesquisar ou 'Sair' para terminar!");
+			cep = leitura.nextLine();
+			
+			if (cep.equalsIgnoreCase("sair")) {
+				break;
+			}
+
+			Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+
+			if (!novoEndereco.erro()) {
+				try {
+
+					System.out.println(novoEndereco);
+					GeradorDeArquivo gerador = new GeradorDeArquivo();
+					gerador.salvaJson(novoEndereco);
+
+				} catch (RuntimeException | IOException e) {
+					System.out.println("ERRO: " + e.getMessage());
+					System.out.println("Programa finalizado no catch da Principal!");
+				}
+			} else {
+				System.out.println("O CEP digitado não existe no banco de dados!");
+			}
 		}
-        
-        System.out.println("Programa finalizado na Principal");
-        
-        leitura.close();
+
+		System.out.println("Programa finalizado na Principal");
+
+		leitura.close();
 	}
-	
+
 }
